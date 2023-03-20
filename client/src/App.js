cd0d953666f39a87be7c1a9cb16b29d043b1743c
kimsj <sophia10047600@gmail.com>
2023-03-20 17:41:59 +0900

    hi

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
 import "./App.css";
+import { useNavigate } from "react-router";
+import SellModalContain from "./component/sell modal/Container/SellModalContain";
+import UserContainer from "./component/User/UserContainer";
+import ReactModal from "react-modal";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import MainCont from "./component/mainPage/Container";
 import GlobalStyle from "./styles/globalStyles";
-import { lightTheme, darkTheme } from "./styles/theme";
+import { lightTheme, darkTheme, Theme } from "./styles/theme";
 import { useSelector } from "react-redux";
 
 ReactModal.setAppElement("#root");
 function App() {
-  return <div className="App">l</div>;
+  const theme = useSelector((state) => state.theme);
+  return (
+    <div className="App">
+      <GlobalStyle
+        theme={theme == "dark" ? darkTheme : lightTheme}
+      ></GlobalStyle>
+      <Routes>
+        <Route path="/" element={<MainCont></MainCont>}></Route>
+        <Route path="/:params" element={<MainCont></MainCont>}></Route>
+        <Route path="/login" element={<UserContainer />}></Route>
+        <Route path="/sellmodal" element={<SellModalContain />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
-
-const AppBox = styled.div`
-  width: 100%;
-  box-sizing: border-box;
-`;
