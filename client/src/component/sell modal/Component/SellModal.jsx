2c9f20193a692b6d0e98c6151ed33790c03859ba
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-03-21 01:12:26 +0900

    23.03.21
    빡시게 살자

--- client/src/component/sell modal/Component/SellModal.jsx
+++ client/src/component/sell modal/Component/SellModal.jsx
@@ -1,7 +1,21 @@
 // import styled from "react-styled";
 import { SiMaterialdesign } from "react-icons/si";
 import { BiFoodMenu } from "react-icons/bi";
-import { AiOutlineTeam, AiOutlineMenu } from "react-icons/ai";
+
+import {
+  AiOutlineTeam,
+  AiOutlineMenu,
+  AiOutlineThunderbolt,
+} from "react-icons/ai";
+import { MdEditAttributes, MdTimeline } from "react-icons/md";
+
+import {
+  BsShareFill,
+  BsThreeDots,
+  BsEye,
+  BsArrowThroughHeart,
+} from "react-icons/bs";
+import { TbTriangleSquareCircle, TbArrowBarToDown } from "react-icons/tb";
 
 import Modal from "react-modal";
 import ReactModal from "react-modal";
@@ -28,49 +42,161 @@ const SellModal = ({ isOpen, onCancel }) => {
           <div>
             <AiOutlineMenu size={"30"} />
             <div>description</div>
+            <div>
+              <TbArrowBarToDown size={"30"} />
+            </div>
           </div>
           <div>
             <AiOutlineTeam size={"30"} />
             By Team Name
+            <div>
+              <TbArrowBarToDown size={"30"} />
+            </div>
           </div>
-          <div>Attributes</div>
-          <div>만들어진 구성들 박스에 담아서..</div>
+          <div>
+            <div>
+              <MdEditAttributes size={"30"} />
+              Attributes
+              <div>
+                <TbArrowBarToDown size={"30"} />
+              </div>
+            </div>
+            <div>
+              <div>box1</div>
+              <div>box2</div>
+              <div>box3</div>
+              <div>box4</div>
+            </div>
+          </div>
+
           <div>
             <BiFoodMenu size={"30"} />
             About seller Team
+            <div>
+              <TbArrowBarToDown size={"30"} />
+            </div>
           </div>
           <div>
             <SiMaterialdesign size={"30"} />
             details
+            <div>
+              <TbArrowBarToDown size={"30"} />
+            </div>
           </div>
         </div>
         <div>
-          <div>판매자</div>
-          <div>판매할 NFT#123</div>
-          <div>view , favorite PFPs</div>
-          <div>time</div>
           <div>
-            ETH
-            <div>addcart</div>
-            <div>make offer</div>
+            판매자{" "}
+            <div>
+              <BsShareFill size={"30"} /> <BsThreeDots size={"30"} />
+            </div>
           </div>
-          <div>grap</div>
-          <div>Listing</div>
-          <div>Offers</div>
-        </div>
+          <div>
+            판매할 NFT#123
+            <div>Ownd by 45666</div>
+          </div>
 
-        <div>
-          <button onClick={() => {}}>확인</button>
-
-          <button
-            onClick={() => {
-              onCancel();
-            }}
-          >
-            취소
-          </button>
+          <div>
+            <div>
+              {" "}
+              <BsEye size={"30"} />
+              view
+            </div>
+            <div>
+              <BsArrowThroughHeart size={"30"} />
+              favorites
+            </div>
+            <div>
+              <TbTriangleSquareCircle size={"30"} /> favorite PFPs
+            </div>
+          </div>
+          <div>
+            <div>sale ends (year)년 (month)월 (days)일 </div>
+            <div>
+              <div>
+                Hour <div>time(Hour)</div>
+              </div>
+              <div>
+                Minutes <div>time(Minutes)</div>
+              </div>
+              <div>
+                Seconds <div>time(Seconds)</div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>Current price</div>
+              <div>$달라 ETH</div>
+            </div>
+            <div>
+              <div>
+                <button>addcart</button>
+              </div>
+              <div>
+                <button>make offer</button>{" "}
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>
+                <MdTimeline size={"30"} />
+              </div>
+              <div>Price History</div>
+            </div>
+            <div>Grap</div>
+          </div>
+          <div>
+            <div>Listing</div>
+            <div>
+              <div>Price</div>
+              <div>USD Price</div>
+              <div>Expiration</div>
+              <div>From</div>
+            </div>
+            <div>
+              <div>Price :Eth</div>
+              <div>USD Price :$</div>
+              <div>Expiration :in number hours</div>
+              <div>Form : ID</div>
+            </div>
+            <div>
+              <button>
+                <AiOutlineThunderbolt size={"20"} />
+                Buy
+              </button>
+            </div>
+          </div>
+          <div>
+            <div>Offers</div>
+            <div>
+              <div>Price</div>
+              <div>USD Price</div>
+              <div>Expiration</div>
+              <div>From</div>
+            </div>
+            <div>
+              <div>Price :Eth</div>
+              <div>USD Price :$</div>
+              <div>Expiration :in number hours</div>
+              <div>Form : ID</div>
+            </div>
+          </div>
         </div>
       </ModalBox>
+
+      <Okbutton>
+        <button onClick={() => {}}>확인</button>
+
+        <button
+          onClick={() => {
+            onCancel();
+          }}
+        >
+          취소
+        </button>
+      </Okbutton>
     </ReactModal>
   );
 };
@@ -79,75 +205,139 @@ export default SellModal;
 const ModalBox = styled.div`
   display: flex;
   width: 100%;
-  background-image: linear-gradient(0.47turn, #e2938f, #ae7dbe, #6f67f1);
+  background-color: rgb(245, 245, 245);
+  /* background-image: linear-gradient(0.47turn, #e2938f, #ae7dbe, #6f67f1); */
+  justify-content: space-around;
   font-size: 20px;
   & > div:first-child {
     display: flex;
     flex-direction: column;
     width: 45%;
-    border: 1px solid red;
+    justify-content: space-around;
+
     & > div:first-child {
       display: flex;
       align-items: center;
       font-weight: 50px;
       height: 42px;
       width: 100%;
-      /* background-color: beige; */
       justify-content: space-between;
+      border: 1px solid rgba(53, 56, 64, 0.3);
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      border-bottom: none;
       & > div:first-child {
         display: flex;
-        width: 100%;
-
+        width: 7%;
+        margin-left: 3%;
         & > img {
           display: flex;
-          width: 15%;
+          width: 100%;
         }
       }
       & > div:nth-child(2) {
         display: flex;
-        width: 100%;
+        width: 7%;
+        margin-left: 55%;
+
         flex-direction: row-reverse;
         & > img {
           display: flex;
-          width: 15%;
+          width: 100%;
         }
       }
       & > div:nth-child(3) {
         display: flex;
-        width: 100%;
+        width: 7%;
         flex-direction: row-reverse;
+        margin-right: 5%;
+
         & > img {
           display: flex;
-          width: 15%;
+          width: 100%;
         }
       }
     }
     & > div:nth-child(2) {
       display: flex;
       width: 100%;
+      border: 1px solid rgba(53, 56, 64, 0.3);
+      border-top: none;
+      justify-content: center;
+
       & > img {
-        width: 100%;
+        display: flex;
+        border-radius: 10px;
+        width: 80%;
       }
     }
     & > div:nth-child(3) {
       display: flex;
-      width: 80%;
-      /* background-color: red; */
+      width: 100%;
       flex-direction: row;
       font-size: 20px;
+      border: 1px solid rgba(53, 56, 64, 0.3);
+
       & > div:first-child img {
         display: flex;
         width: 60%;
       }
+      & > div:last-child {
+        display: flex;
+
+        margin-left: 60%;
+      }
+    }
+    & > div:nth-child(4) {
+      display: flex;
+      border: 1px solid rgba(53, 56, 64, 0.3);
+      & > div:last-child {
+        display: flex;
+
+        margin-left: 52.3%;
+      }
     }
     & > div:nth-child(5) {
       display: flex;
-      width: 40%;
+      width: 100%;
       font-size: 20px;
+      border: 1px solid rgba(53, 56, 64, 0.3);
+      flex-direction: column;
+
+      & > div:first-child {
+        display: flex;
+        flex-direction: row;
+        & > div:last-child {
+          display: flex;
+
+          margin-left: 62.6%;
+        }
+      }
+      & > div:last-child {
+        display: flex;
+      }
 
       & > div img {
         display: flex;
         width: 38%;
+      }
+    }
+    & > div:nth-child(6) {
+      display: flex;
+      border: 1px solid rgba(53, 56, 64, 0.3);
+      & > div:last-child {
+        display: flex;
+
+        margin-left: 46.7%;
+      }
+    }
+    & > div:nth-child(7) {
+      display: flex;
+      border: 1px solid rgba(53, 56, 64, 0.3);
+      & > div:last-child {
+        display: flex;
+
+        margin-left: 69.2%;
       }
     }
   }
@@ -155,10 +345,211 @@ const ModalBox = styled.div`
     display: flex;
     width: 50%;
     flex-direction: column;
-    border: 1px solid blue;
+    justify-content: space-around;
+    font-size: 30px;
+
     & > div:first-child {
       display: flex;
-      width: 14%;
+      width: 100%;
+      justify-content: space-between;
+      & > div {
+        display: flex;
+        gap: 13px;
+      }
+    }
+    & > div:nth-child(2) {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      text-align: center;
+      & > div:first-child {
+        display: flex;
+        font-size: 20px;
+        justify-content: space-around;
+      }
+    }
+    & > div:nth-child(3) {
+      display: flex;
+      font-size: 28px;
+      justify-content: space-around;
+    }
+    & > div:nth-child(4) {
+      display: flex;
+      flex-direction: column;
+      font-size: 22px;
+      & > div:nth-child(2) {
+        display: flex;
+        justify-content: space-around;
+        font-size: 30px;
+      }
+    }
+    & > div:nth-child(5) {
+      display: flex;
+      width: 100%;
+      font-size: 16px;
+
+      flex-direction: column;
+      & > div:first-child {
+        display: flex;
+        flex-direction: column;
+        font-size: 28px;
+
+        & > div:first-child {
+          display: flex;
+          font-size: 20px;
+        }
+      }
+      & > div:nth-child(2) {
+        display: flex;
+        width: 100%;
+        justify-content: space-around;
+        & > div button {
+          transition: 0.6s cubic-bezier(0.77, 0, 0.175, 1); // ease-in-out-quartic;
+
+          &::before {
+            position: absolute;
+            content: "";
+            top: 0;
+            left: 0;
+            z-index: -1;
+            width: 100%;
+            height: 100%;
+            background: var(--btn-bg);
+            transform: scaleX(0);
+            transform-origin: right;
+            transition: transform 0.6s cubic-bezier(0.77, 0, 0.175, 1);
+          }
+
+          &:hover {
+            color: white;
+            background: transparent;
+            background-image: linear-gradient(
+              0.47turn,
+              #e2938f,
+              #ae7dbe,
+              #6f67f1
+            );
+
+            &::before {
+              transform: scaleX(1);
+              transform-origin: left;
+            }
+          }
+        }
+      }
+    }
+    & > div:nth-child(6) {
+      display: flex;
+      flex-direction: column;
+      & > div:first-child {
+        display: flex;
+      }
+    }
+    & > div:nth-child(7) {
+      display: flex;
+      flex-direction: column;
+
+      & > div:first-child {
+        display: flex;
+      }
+      & > div:nth-child(2) {
+        display: flex;
+        width: 80%;
+        justify-content: space-around;
+      }
+      & > div:nth-child(3) {
+        display: flex;
+        width: 80%;
+        justify-content: space-around;
+        font-size: 15px;
+      }
+      & > div:nth-child(4) {
+        display: flex;
+        flex-direction: row-reverse;
+        font-size: 15px;
+      }
+      & > div button {
+        transition: 0.6s cubic-bezier(0.77, 0, 0.175, 1); // ease-in-out-quartic;
+
+        &::before {
+          position: absolute;
+          content: "";
+          top: 0;
+          left: 0;
+          z-index: -1;
+          width: 100%;
+          height: 100%;
+          background: var(--btn-bg);
+          transform: scaleX(0);
+          transform-origin: right;
+          transition: transform 0.6s cubic-bezier(0.77, 0, 0.175, 1);
+        }
+
+        &:hover {
+          color: white;
+          background: transparent;
+          background-image: linear-gradient(
+            0.47turn,
+            #e2938f,
+            #ae7dbe,
+            #6f67f1
+          );
+
+          &::before {
+            transform: scaleX(1);
+            transform-origin: left;
+          }
+        }
+      }
+    }
+    & > div:nth-child(8) {
+      display: flex;
+      flex-direction: column;
+
+      & > div:nth-child(2) {
+        display: flex;
+        justify-content: space-around;
+      }
+      & > div:nth-child(3) {
+        display: flex;
+        justify-content: space-around;
+        font-size: 18px;
+      }
+    }
+  }
+`;
+const Okbutton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > button {
+    width: 18%;
+
+    transition: 0.6s cubic-bezier(0.77, 0, 0.175, 1); // ease-in-out-quartic;
+
+    &::before {
+      position: absolute;
+      content: "";
+      top: 0;
+      left: 0;
+      z-index: -1;
+      width: 100%;
+      height: 100%;
+      background: var(--btn-bg);
+      transform: scaleX(0);
+      transform-origin: right;
+      transition: transform 0.6s cubic-bezier(0.77, 0, 0.175, 1);
+    }
+
+    &:hover {
+      color: white;
+      background: transparent;
+      background-image: linear-gradient(0.47turn, #e2938f, #ae7dbe, #6f67f1);
+
+      &::before {
+        transform: scaleX(1);
+        transform-origin: left;
+      }
     }
   }
 `;
