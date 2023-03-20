36007fbf9f30b391295bab11db083de491d0836c
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-03-20 17:00:05 +0900

    모달 창 1/3

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import { useNavigate } from "react-router";
+import SellModalContain from "./component/sell modal/Container/SellModalContain";
+import ReactModal from "react-modal";
+ReactModal.setAppElement("#root");
 function App() {
-  return <div className="App"></div>;
+  // const nav = useNavigate();
+  return (
+    <div className="App">
+      <SellModalContain />
+    </div>
+  );
 }
 
 export default App;
